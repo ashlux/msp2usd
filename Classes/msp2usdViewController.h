308f7082e8aef079a7b259332343a3ee76812fa6
Ash Lux <ashlux@gmail.com>
2009-06-01 00:32:19 -0500

    add other currencies

--- Classes/msp2usdViewController.h
+++ Classes/msp2usdViewController.h
@@ -2,14 +2,35 @@
 
 @interface msp2usdViewController : UIViewController {
 	IBOutlet UITextField *pointsTextField;
-	IBOutlet UITextField *usdTextField;
+	IBOutlet UITextField *currencyTextField;
+	
+	IBOutlet UILabel *pointsToCurrencyLabel;
+	IBOutlet UILabel *currencyToPointsLabel;
+	IBOutlet UIButton *convertToCurrencyButton;
+	IBOutlet UILabel *currencyLabel;
+	
+	NSString *selectedCurrency;
 }
 
 @property (nonatomic, retain) IBOutlet UITextField *pointsTextField;
-@property (nonatomic, retain) IBOutlet UITextField *usdTextField;
+@property (nonatomic, retain) IBOutlet UITextField *currencyTextField;
+
+@property (nonatomic, retain) IBOutlet UILabel *pointsToCurrencyLabel;
+@property (nonatomic, retain) IBOutlet UILabel *currencyToPointsLabel;
+@property (nonatomic, retain) IBOutlet UIButton *convertToCurrencyButton;
+@property (nonatomic, retain) IBOutlet UILabel *currencyLabel;
+
+@property (nonatomic, retain) NSString *selectedCurrency;
 
 -(IBAction) convertToPoints;
--(IBAction) convertToDollars;
+-(IBAction) convertToCurrency;
+
+-(IBAction) selectAUD;
+-(IBAction) selectCAD;
+-(IBAction) selectEUR;
+-(IBAction) selectGBP;
+-(IBAction) selectJPY;
+-(IBAction) selectUSD;
 
 -(IBAction) sourceOnGitHub;
 
--- Classes/msp2usdViewController.m
+++ Classes/msp2usdViewController.m
@@ -3,33 +3,124 @@
 @implementation msp2usdViewController
 
 @synthesize pointsTextField;
-@synthesize usdTextField;
+@synthesize currencyTextField;
+@synthesize pointsToCurrencyLabel;
+@synthesize currencyToPointsLabel;
+@synthesize convertToCurrencyButton;
+@synthesize currencyLabel;
 
+@synthesize selectedCurrency;
+
+double rateCurrencyToPoints;
+double ratePointsToCurrency;
+
+-(void) setUiForCurrency:(NSString*)currency {
+	[pointsToCurrencyLabel setText:[NSString stringWithFormat:@"Microsoft Points to %@", currency]];
+	[currencyToPointsLabel setText:[NSString stringWithFormat:@"%@ to Microsoft Points", currency]];
+	[convertToCurrencyButton setTitle:[NSString stringWithFormat:@"Convert to %@", currency] forState:UIControlStateNormal];
+	[currencyLabel setText:[NSString stringWithFormat:@"%@:", currency]];
+}
+
+-(IBAction) selectAUD {
+	selectedCurrency = @"AUD";
+	rateCurrencyToPoints = 0.0165;
+	ratePointsToCurrency = 60.606060;
+	[self setUiForCurrency:selectedCurrency];
+}
+
+-(IBAction) selectCAD {
+	selectedCurrency = @"CAD";
+	rateCurrencyToPoints = 0.0155;
+	ratePointsToCurrency =  64.516129;
+	[self setUiForCurrency:selectedCurrency];
+}
+
+-(IBAction) selectEUR {
+	selectedCurrency = @"EUR";
+	rateCurrencyToPoints = 0.012;
+	ratePointsToCurrency = 83.333333;
+	[self setUiForCurrency:selectedCurrency];
+}
+
+-(IBAction) selectGBP {
+	selectedCurrency = @"GBP";
+	rateCurrencyToPoints = 0.0085;
+	ratePointsToCurrency = 117.647059;
+	[self setUiForCurrency:selectedCurrency];
+}
+
+-(IBAction) selectJPY {
+	selectedCurrency = @"JPY";
+	rateCurrencyToPoints = 1.48;
+	ratePointsToCurrency = 0.675675;
+	[self setUiForCurrency:selectedCurrency];
+}
+
+-(IBAction) selectUSD {
+	selectedCurrency = @"USD";
+	rateCurrencyToPoints = 0.0125;
+	ratePointsToCurrency = 80.0;
+	[self setUiForCurrency:selectedCurrency];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	NSLog([defaults objectForKey:@"currency"]);
+	
+	selectedCurrency = [defaults objectForKey:@"currency"];
+	if ([selectedCurrency isEqualToString:@"AUD"]) {
+		[self selectAUD];
+	} else if ([selectedCurrency isEqualToString:@"CAD"]) {
+		[self selectCAD];
+	} else if ([selectedCurrency isEqualToString:@"EUR"]) {
+		[self selectEUR];
+	} else if ([selectedCurrency isEqualToString:@"GBP"]) {
+		[self selectGBP];
+	} else if ([selectedCurrency isEqualToString:@"JPY"]) {
+		[self selectJPY];
+	} else if ([selectedCurrency isEqualToString:@"USD"]) {
+		[self selectUSD];
+	} else if (selectedCurrency == nil) { // default to USD
+		[self selectUSD];
+	}
+	
+	[super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	[defaults setObject:selectedCurrency forKey:@"currency"];
+	[defaults synchronize];
+	
+	[super viewWillDisappear:animated];
+}
 
 -(IBAction) sourceOnGitHub {
 	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://github.com/ashlux/msp2usd/tree/master"]];
 }
 
 -(IBAction) convertToPoints {
-	double usd = [usdTextField.text doubleValue];
-	// TODO: ROUND!
-	double points = usd * 80.0; // 80 points per dollar 
+	double usd = [currencyTextField.text doubleValue];
+	double points = usd * ratePointsToCurrency;
 	
-	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"USD to Points"
-														message:[NSString stringWithFormat:@"$%@ = %0.2f points", usdTextField.text, points]
+	NSString *title = [NSString stringWithFormat:@"%@ to Microsoft Points", selectedCurrency];
+	NSString *message = [NSString stringWithFormat:@"%@ %@ = %0.2f Points", currencyTextField.text, selectedCurrency, points];
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
+														message:message
 													   delegate:nil
 											  cancelButtonTitle:@"Okay" otherButtonTitles:nil];
     [alertView show];
     [alertView release];
 }
 
--(IBAction) convertToDollars {
+-(IBAction) convertToCurrency {
 	double points = [pointsTextField.text doubleValue];
-	// TODO: ROUND!
-	double usd = points * 0.0125; // 1 point = 0.0125 dollars
+	double currency = points * rateCurrencyToPoints;
 	
-	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Points to USD"
-														message:[NSString stringWithFormat:@"%@ points = $%0.2f", pointsTextField.text, usd]
+	NSString *title = [NSString stringWithFormat:@"Microsoft Points to %@", selectedCurrency];
+	NSString *message = [NSString stringWithFormat:@"%@ Points = %0.2f %@", pointsTextField.text, currency, selectedCurrency];
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
+														message:message
 													   delegate:nil
 											  cancelButtonTitle:@"Okay" otherButtonTitles:nil];
     [alertView show];
@@ -43,7 +134,13 @@
 
 - (void)dealloc {
 	[pointsTextField release];
-	[usdTextField release];
+	[currencyTextField release];
+	[selectedCurrency release];
+	[pointsToCurrencyLabel release];
+	[currencyToPointsLabel release];
+	[convertToCurrencyButton release];
+	[currencyLabel release];
+	
 	
     [super dealloc];
 }
